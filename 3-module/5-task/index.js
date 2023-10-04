d736acbe8bd7c78fce2f65ef418a762aae7baac4
Arthur N. Chernikov <artcher@me.com>
2023-10-04 16:05:14 +0300

    Решения с первой по пятую задач третьего модуля

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,13 @@
 function getMinMax(str) {
-  // ваш код...
+  let result = {
+    min: 0,
+    max: 0,
+  };
+  let output = str
+    .split(" ")
+    .filter((item) => Number(item))
+    .sort((a, b) => a - b);
+  result.min = Math.min(...output);
+  result.max = Math.max(...output);
+  return result;
 }
